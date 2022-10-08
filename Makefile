29856872ce6e88752f6f16907644ffe608b51a93
lusantor <lusantor@student.42.rio>
2022-10-08 19:11:46 -0300

    Deletes Makefile and adds utilities

--- Makefile
+++
@@ -1,9 +0,0 @@
-SRC = get_next_line.c
-CFLAGS = -Wall -Wextra -Werror
-
-all: 
-	$(CC) $(CFLAGS) $(SRC)
-
-test: all
-	./a.out
-	$(RM) a.out
--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lusantor <lusantor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 13:04:20 by lusantor          #+#    #+#             */
-/*   Updated: 2022/10/08 15:44:45 by lusantor         ###   ########.fr       */
+/*   Updated: 2022/10/08 19:07:45 by lusantor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,30 @@
 #include <stdlib.h>
 
 // ssize_t	read(int fd, void *buf, size_t count)
-// reads count bytes from fd pointed file into *buf
+// > reads count bytes from fd pointed file into *buf
 
 char *get_next_line (int fd)
 {
-	int		bufa_sai;
-	char	*bufa;
+	char		*bufa;
 
-	bufa_sai = 2048;
-	bufa = malloc(bufa_sai);
-	read(fd, bufa, bufa_sai);
+	bufa = malloc(BUFFER_SIZE);
+	read(fd, bufa, BUFFER_SIZE);
 	return (bufa);
 }
 
 #include <fcntl.h>
 #include <stdio.h>
 int	main(void)
-{	
-	printf("%i\n", open("lulala.txt", O_RDONLY));
-	printf("%s\n", get_next_line(3));
+{
+	int	file_no;
+
+	file_no = open("lulala.txt", O_RDONLY);
+	printf("call 1: %s\n", get_next_line(file_no));
+	printf("call 2: %s\n", get_next_line(file_no));
+	printf("call 3: %s\n", get_next_line(file_no));
+	printf("call 4: %s\n", get_next_line(file_no));
+	printf("call 5: %s\n", get_next_line(file_no));
+	printf("call 6: %s\n", get_next_line(file_no));
+	printf("call 7: %s\n", get_next_line(file_no));
 	return (0);
 }
