255d92f36c9d36b194e64a2921438cee32524dcb
Lucas Santoro <santoro89@gmail.com>
2022-10-20 00:36:42 -0300

    Function crashes at buffer size 30

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -62,18 +62,3 @@ char	*gnl_strchr(const char *s, int c)
 	else
 		return (NULL);
 }
-
-void	*gnl_memcpy(void *dest, const void *src, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	if (dest == NULL && src == NULL)
-		return (NULL);
-	while (i < n)
-	{
-		*((char *)(dest + i)) = *((char *)(src + i));
-		i++;
-	}
-	return (dest);
-}
